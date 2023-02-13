ea9be9ea64e46b4ab292e12625a997384973b84d
hongise20 <hongise20@gmail.com>
2023-02-14 00:15:39 +0900

    2023_0213_followingView_Changed

--- resources/views/follows/followList.blade.php
+++ resources/views/follows/followList.blade.php
@@ -3,12 +3,27 @@
 @section('content')
 
 <div class="post_container">
+<div class ="page_title">Follow Lists</div>
 {{Form::open(['url' => '/post', 'files' => true])}}
-<h1>FollowLists</h1>
+@foreach($userLists as $userPics)
+
+    @if(Auth::user()->isFollowing($userPics->id))
+    
+        @if(Auth::user()->id == $userPics->id)
+            @continue
+        @else
+        <div class="follows_list_images">
+            <a><img src="{{ asset('../images/'.$userPics->images) }}" width="90px" height="90px"></a>
+        </div>
+        @endif
+
+    @else
+
+    @endif
+
+@endforeach
 </div>
 {{Form::close()}}
-
-{{Form::open(['url' => '/post', 'files' => true])}}
 
 
 <div class = "postlists">
@@ -16,13 +31,19 @@
 @foreach($userLists as $userLists)
 
     @if(Auth::user()->isFollowing($userLists->id))
-    @foreach($postLists as $postLists)
-    <div class="post_images">
-        <a><img src="{{ asset('../images/'.$postLists->user->images) }}" width="90px" height="90px"></a>
-    </div>
-    <p>{{$postLists->post}}</p>
-    <p>{{$postLists->created_at}}</p>
-    @endforeach
+    
+        @foreach($postLists as $postLists)
+            @if(Auth::user()->id == $postLists->user->id)
+                @continue
+            @else
+            <div class="post_images">
+                <a><img src="{{ asset('../images/'.$postLists->user->images) }}" width="90px" height="90px"></a>
+            </div>
+            <p>{{$postLists->post}}</p>
+            <p>{{$postLists->created_at}}</p>
+            @endif
+        @endforeach
+
     @else
 
     @endif
@@ -30,6 +51,5 @@
 @endforeach
 </div>
 
-{{Form::close()}}
 
 @endsection
--- resources/views/follows/followerList.blade.php
+++ resources/views/follows/followerList.blade.php
@@ -1,4 +1,54 @@
 @extends('layouts.login')
 
 @section('content')
+
+<div class="post_container">
+<div class ="page_title">Follower Lists</div>
+{{Form::open(['url' => '/post', 'files' => true])}}
+@foreach($userLists as $userPics)
+
+    @if(Auth::user()->isFollowed($userPics->id))
+    
+        @if(Auth::user()->id == $userPics->id)
+            @continue
+        @else
+        <div class="follows_list_images">
+            <a><img src="{{ asset('../images/'.$userPics->images) }}" width="90px" height="90px"></a>
+        </div>
+        @endif
+
+    @else
+
+    @endif
+
+@endforeach
+</div>
+{{Form::close()}}
+
+
+<div class = "postlists">
+
+@foreach($userLists as $userLists)
+
+    @if(Auth::user()->isFollowed($userLists->id))
+    
+        @foreach($postLists as $postLists)
+            @if(Auth::user()->id == $postLists->user->id)
+                @continue
+            @else
+            <div class="post_images">
+                <a><img src="{{ asset('../images/'.$postLists->user->images) }}" width="90px" height="90px"></a>
+            </div>
+            <p>{{$postLists->post}}</p>
+            <p>{{$postLists->created_at}}</p>
+            @endif
+        @endforeach
+
+    @else
+
+    @endif
+
+@endforeach
+</div>
+
 @endsection
